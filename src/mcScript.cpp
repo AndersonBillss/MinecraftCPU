e0623880d4889393daf65293590d452ceadcae8d
AndersonBillss <anderson@thebillsfamily.com>
2025-06-27 13:06:24 -0600

    Changed output for no arguments

--- src/mcScript.cpp
+++ src/mcScript.cpp
@@ -2,17 +2,28 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <unordered_map>
 #include "assembler/assembler.hpp"
 #include "utils/stringUtils.hpp"
 #include "utils/binUtils.hpp"
 #include "utils/fileUtils.hpp"
 #include "utils/syntaxError.hpp"
 
+// no flags (empty)
+//      -o
+//      -o-schem
+//
+// -compile
+//      -o
+//
+// -assemble
+//      -o
+//      -o-schem
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-        std::cerr << "No file provided" << std::endl;
+        std::cerr << "No Aguments provided\nUse the -help flag to see available commands" << std::endl;
         return 1;
     }
     std::string fileName = argv[1];
@@ -29,7 +40,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    std::string filePath = "sampleFile//hello/test.txt";
+    std::string filePath = "sampleFile//hello/test.txt/";
     std::string contents = "HELLO, WORLD!";
     try
     {
