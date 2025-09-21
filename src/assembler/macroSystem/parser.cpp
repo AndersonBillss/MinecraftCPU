6ed826f4cc87be17b81c5eb8ca7342397d72a101
AndersonBillss <anderson@thebillsfamily.com>
2025-09-21 14:35:20 -0600

    Partially implemented groupParentheses function

--- src/assembler/macroSystem/parser.cpp
+++ src/assembler/macroSystem/parser.cpp
@@ -1,9 +1,29 @@
 #include "parser.hpp"
 
+std::vector<std::unique_ptr<Parser::AST>> groupParentheses(std::vector<AsmMacroLexer::Token> tokens, size_t startingIndex = 0)
+{
+    std::vector<std::unique_ptr<Parser::AST>> parseResult = {};
+    for (size_t i = startingIndex; i < tokens.size(); i++)
+    {
+        AsmMacroLexer::Token token = tokens[i];
+        if (token.type == AsmMacroLexer::TokenType::OPENINGPARENTHESE)
+        {
+            Parser::AST tree = {
+                token.begin,
+                0,
+                Parser::NodeType::BLOCK,
+                groupParentheses(tokens, i),
+                std::string(""),
+                0,
+            };
+        }
+    }
+}
 
 std::vector<std::unique_ptr<Parser::AST>> Parser::parseTokens(std::vector<AsmMacroLexer::Token> tokens)
 {
-    std::vector<std::unique_ptr<AST>> tree;
+    size_t blockBaseIndex = 0;
+    std::vector<std::unique_ptr<Parser::AST>> tree = {};
 
     for (size_t i = 0; i < tokens.size() - 1; i++)
     {
