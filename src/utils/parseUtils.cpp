ea77bbbf8125766cdd47d448ed9df7f81dd0bc3e
AndersonBillss <anderson@thebillsfamily.com>
2025-10-15 14:16:54 -0600

    Implemented parseInt function

--- src/utils/parseUtils.cpp
+++ src/utils/parseUtils.cpp
@@ -1,4 +1,5 @@
 #include "parseUtils.hpp"
+#include "stringUtils.hpp"
 #include <unordered_set>
 
 bool ParseUtils::isNumeric(std::string text)
@@ -16,10 +17,40 @@ bool ParseUtils::isNumeric(std::string text)
         '8',
         '9',
     };
-    return false;
+    auto it = numericChars.find(text[0]);
+    return it != numericChars.end();
+}
+
+int parseHexadecimal(std::string text, bool isSigned)
+{
+    return 0;
+}
+int parseBinary(std::string text, bool isSigned)
+{
+    return 0;
+}
+int parseDecimal(std::string text)
+{
+    return 0;
 }
 
 int ParseUtils::parseInt(std::string text, bool isSigned)
 {
-    return 0;
+    bool isBinOrHex = stringUtils::subSectionEqual(text, 0, "0");
+    bool isHex = stringUtils::subSectionEqual(text, 1, "x") || stringUtils::subSectionEqual(text, 1, "X");
+    isHex = isBinOrHex && isHex;
+    bool isBin = stringUtils::subSectionEqual(text, 1, "b") || stringUtils::subSectionEqual(text, 1, "B");
+    isBin = isBinOrHex && isHex;
+    if (isHex)
+    {
+        return parseHexadecimal(text, isSigned);
+    }
+    else if (isBin)
+    {
+        return parseBinary(text, isSigned);
+    }
+    else
+    {
+        return parseDecimal(text);
+    }
 }
