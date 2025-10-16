64ecdfee476f1cf21b8e083ecf85eb332ae2a28b
AndersonBillss <anderson@thebillsfamily.com>
2025-10-16 09:29:36 -0600

    Added more tests for parsing hexadecimal numbers

--- test/parseUtils.cpp
+++ test/parseUtils.cpp
@@ -3,7 +3,7 @@
 #include "catch_amalgamated.hpp"
 #include "../src/utils/parseUtils.hpp"
 
-TEST_CASE("Parse hexadecimal integers")
+TEST_CASE("Parse unsigned hexadecimal integers")
 {
     std::string hexString = "0x3";
     int expected = 0x3;
@@ -11,5 +11,21 @@ TEST_CASE("Parse hexadecimal integers")
 
     hexString = "0x25";
     expected = 0x25;
+    REQUIRE(ParseUtils::parseInt(hexString, 32) == expected);
+
+    hexString = "0X8F3";
+    expected = 0X8F3;
+    REQUIRE(ParseUtils::parseInt(hexString, 32) == expected);
+    
+    hexString = "0X8F3";
+    expected = 0X8F3;
+    REQUIRE(ParseUtils::parseInt(hexString, 32) == expected);
+
+    hexString = "0Xabcdef";
+    expected = 0Xabcdef;
+    REQUIRE(ParseUtils::parseInt(hexString, 32) == expected);
+
+    hexString = "0x3f19ad6";
+    expected = 0x3f19ad6;
     REQUIRE(ParseUtils::parseInt(hexString, 32) == expected);
 }
